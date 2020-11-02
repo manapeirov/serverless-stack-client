6677ebcc17eacceed9a0c3c6b9b86a42a1e10484
Mousavi Jalali <mana.mousavi.jalali@accenture.com>
2020-11-02 18:32:32 +0000

    changing AWS config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const dev = {
     "pk_test_51HdyzhB7dJPV96N9YgSWTTujEJu9MwJiCGlEbEjrLJpHcAiUvh5ValInmdP1AzJlc9UcL98dWmYX4kOlXWLEWtsH00zHelb4Pn\n",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "dev-mana-notes-infrastructure-s3-uploads4f6eb0fd-1uj1w21yfl5zt",
+    BUCKET: "dev-mana-notes-infra-s3-uploads4f6eb0fd-c5wljmm4g08v",
   },
   apiGateway: {
     REGION: "us-east-1",
@@ -11,9 +11,9 @@ const dev = {
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_vt5sCrSHU",
-    APP_CLIENT_ID: "419gc3vi07p9n2r9qbcnqcc7f8",
-    IDENTITY_POOL_ID: "us-east-1:e0af568d-86af-48b7-b820-b87dc4590620",
+    USER_POOL_ID: "us-east-1_2KcYDFYnn",
+    APP_CLIENT_ID: "3bbi2ngca6ut6ctopog6pvtvpp",
+    IDENTITY_POOL_ID: "us-east-1:ed5b4081-7529-42cf-b14d-8937a1e77130",
   },
 };
 
@@ -22,17 +22,17 @@ const prod = {
     "pk_test_51HdyzhB7dJPV96N9YgSWTTujEJu9MwJiCGlEbEjrLJpHcAiUvh5ValInmdP1AzJlc9UcL98dWmYX4kOlXWLEWtsH00zHelb4Pn\n",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "prod-mana-notes-infrastructure-s3-uploads4f6eb0fd-oiv5eac7qia6",
+    BUCKET: "prod-mana-notes-infra-s3-uploads4f6eb0fd-id2wrd5ncmr9",
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://ez0de5li8h.execute-api.us-east-1.amazonaws.com/prod",
+    URL: "https://api.mana-serverless-app.co.uk/prod",
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_15GBaxUYK",
-    APP_CLIENT_ID: "4gjoq5j4l31scv97hv9bcucbn1",
-    IDENTITY_POOL_ID: "us-east-1:83e9673d-8ace-41a8-ab47-3f7a95631dc5",
+    USER_POOL_ID: "us-east-1_5sPbeNl0e",
+    APP_CLIENT_ID: "7kbgdfumcbmjlucdvdfpo6g7dg",
+    IDENTITY_POOL_ID: "us-east-1:234f93a8-1652-4d9b-9c44-4fa387a4be33",
   },
 };
 
